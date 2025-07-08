e5de9ef88843b72964608f09b9d3ac43badc82a3
Kalooga63Alexamder <Kaloog1979@gmail.com>
2025-07-08 18:01:09 +0400

    code coverage by branch

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -10,8 +10,8 @@ public class StatisticsServiceTest {
     void findMax() {
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 12;
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 43, 8, 6, 11, 11, 12};
+        long expected = 43;
 
         long actual = service.findMax(incomesInBillions);
 
